9afae1afd89a4baad3751acddfdf6d6bca9d7b2d
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-06 02:26:01 -0700

    Header Part 1

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/macro';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+export const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
+    no-repeat;
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
+  }
+`;
+
+export const Frame = styled.div``;
+
+export const Container = styled.div``;
+
+export const Logo = styled.img``;
+
+export const ButtonLink = styled(ReactRouterLink)`
+  display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
+  &:hover {
+    background: #f40612;
+  }
+`;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 
 export default function Home() {
-    return (
-        <>
+  return (
+    <>
+      <HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
         <FooterContainer />
-        </>
-    )
+      </HeaderContainer>
+    </>
+  );
 }
