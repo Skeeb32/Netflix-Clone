09f6f01894dd192de2488814fb4be24e0787b2e9
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-06 02:53:27 -0700

    Header Part 2

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg'
 
 export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="Netflix" />
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo}/>
         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
       {children}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Feature, OptForm } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
@@ -8,10 +9,28 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programs and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at anytime.
+          </Feature.SubTitle>
+     
+        <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>   
+          </Feature>
       </HeaderContainer>
+
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </>
   );
 }
