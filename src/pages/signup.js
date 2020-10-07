6be7eb80f93e72f16ab4826c1f2820c1f64dc7fe
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-07 00:50:11 -0700

    Registration Page, Routes Debugging

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -6,7 +6,7 @@ import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import * as ROUTES from '../constants/routes';
 
-export default function Signup() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
@@ -14,10 +14,31 @@ export default function Signup() {
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+
   const isInvalid = firstName === '' || password === '' || emailAddress === '';
 
   const handleSignup = (event) => {
     event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
   };
 
   return (
