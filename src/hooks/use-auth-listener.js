c6f4dc2207409a9d4c1998679bd912aa929d03f8
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-07 17:58:57 -0700

    Header Update, Loading, Player, Profile, Hooks

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,9 +2,7 @@ import { useState, useEffect, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
 
 export default function useAuthListener() {
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem('authUser'))
-  );
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
@@ -17,6 +15,7 @@ export default function useAuthListener() {
         setUser(null);
       }
     });
+
     return () => listener();
   }, []);
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,19 +10,14 @@ export default function Home() {
     <>
       <HeaderContainer>
         <Feature>
-          <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
-          <Feature.SubTitle>
-            Watch anywhere. Cancel at anytime.
-          </Feature.SubTitle>
-
+          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
           <OptForm>
             <OptForm.Input placeholder="Email address" />
-            <OptForm.Button>Try it now</OptForm.Button>{' '}
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
           </OptForm>
-          <OptForm.Text>
-            Ready to watch? Enter your email to create or restart your
-            membership
-          </OptForm.Text>
         </Feature>
       </HeaderContainer>
 
