1c00502042c6b30b04d25cf8ffec19f3169bf311
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-07 23:25:29 -0700

    Accordion Test

--- src/__tests__/components/player.test.js
+++ src/__tests__/components/player.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import { Player } from '../components'
+import { Player } from '../../components';
 
 describe('<Player />', () => {
   it('renders the <Player /> with a bunny video', () => {
