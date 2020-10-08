d1b690a214eb26b45c875ff0e1493c70f78960ad
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-10-07 19:06:08 -0700

    Sign Out in Browser

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Header, Profiles } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
+import { FirebaseContext } from '../context/firebase';
 
 export function SelectProfileContainer({ user, setProfile }) {
+  const { firebase } = useContext(FirebaseContext);
+  const signOut = firebase.auth().currentUser || {};
+
   return (
     <>
       <Header bg={false}>
         <Header.Frame>
           <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+          <Header.Text>
+            <Header.Group>
+              <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                Sign Out
+              </Header.TextLink>
+            </Header.Group>
+          </Header.Text>
         </Header.Frame>
       </Header>
 
@@ -16,7 +27,12 @@ export function SelectProfileContainer({ user, setProfile }) {
         <Profiles.Title>Who's watching?</Profiles.Title>
         <Profiles.List>
           <Profiles.User
-            onClick={() => setProfile({ displayName: user.displayName, photoURL: user.photoURL })}
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
             data-testid="user-profile"
           >
             <Profiles.Picture src={user.photoURL} />
